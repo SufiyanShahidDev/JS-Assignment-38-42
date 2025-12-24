296493c3683cca057a55c308725bda01bf5f0012
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2025-12-24 12:04:04 +0500

    Initial commit

--- app.js
+++ app.js
@@ -112,28 +112,109 @@
 
 // Answer6
 
-function removeVowels(sentence) {
-    var result = "";
+// function removeVowels(sentence) {
+//     var result = "";
 
-    for (var i = 0; i < sentence.length; i++) {
-        var char = sentence[i];
+//     for (var i = 0; i < sentence.length; i++) {
+//         var char = sentence[i];
 
-        if (
-            char !== "a" && char !== "e" && char !== "i" &&
-            char !== "o" && char !== "u" &&
-            char !== "A" && char !== "E" && char !== "I" &&
-            char !== "O" && char !== "U"
-        ) {
-            result = result + char;
-        }
-    }
+//         if (
+//             char !== "a" && char !== "e" && char !== "i" &&
+//             char !== "o" && char !== "u" &&
+//             char !== "A" && char !== "E" && char !== "I" &&
+//             char !== "O" && char !== "U"
+//         ) {
+//             result = result + char;
+//         }
+//     }
 
-    return result;
+//     return result;
+// }
+
+// var userSentence = prompt("Enter a sentence (max 25 characters):");
+
+// var output = removeVowels(userSentence);
+
+// alert("Sentence without vowels:\n" + output);
+// console.log(output);
+
+// Answer7
+
+// function countDoubleVowels(text) {
+//     var count = 0;
+
+//     for (var i = 0; i < text.length - 1; i++) {
+//         var pair = text[i] + text[i + 1];
+
+//         switch (pair) {
+//             case "aa":
+//             case "ae":
+//             case "ai":
+//             case "ao":
+//             case "au":
+
+//             case "ea":
+//             case "ee":
+//             case "ei":
+//             case "eo":
+//             case "eu":
+
+//             case "ia":
+//             case "ie":
+//             case "ii":
+//             case "io":
+//             case "iu":
+
+//             case "oa":
+//             case "oe":
+//             case "oi":
+//             case "oo":
+//             case "ou":
+
+//             case "ua":
+//             case "ue":
+//             case "ui":
+//             case "uo":
+//             case "uu":
+//                 count++;
+//                 break;
+//         }
+//     }
+
+//     return count;
+// }
+
+// var sentence = prompt("Enter a sentence:");
+// var result = countDoubleVowels(sentence.toLowerCase());
+
+// alert("Number of two-vowel occurrences: " + result);
+// console.log(result);
+
+// Answer8
+
+var distanceKm = +prompt("Enter distance between two cities (in kilometers):");
+
+function convertToMeters(km) {
+    var meters = km * 1000;
+    alert("Distance in meters: " + meters);
 }
 
-var userSentence = prompt("Enter a sentence (max 25 characters):");
+function convertToFeet(km) {    
+    var feet = km * 3280.84;
+    alert("Distance in feet: " + feet);
+}
 
-var output = removeVowels(userSentence);
+function convertToInches(km) {
+    var inches = km * 39370.1;
+    alert("Distance in inches: " + inches);
+}
 
-alert("Sentence without vowels:\n" + output);
-console.log(output);
+function convertToCentimeters(km) {
+    var centimeters = km * 100000;
+    alert("Distance in centimeters: " + centimeters);
+}
+
+convertToMeters(distanceKm);
+convertToFeet(distanceKm);
+convertToInches(distanceKm);
+convertToCentimeters(distanceKm);
